0fc4c743502a01e1828fbd782b168a1bd2bf71a6
Elvin <engrioelvin@gmail.com>
2026-01-15 23:08:30 +0700

    Created API, Routing & Connection to the database

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,6 +1,18 @@
+"use client"
+
+import CardPopularProducts from "./CardPopularProducts"
+
 function Dashboard() {
     return (
-        <div>Dashboard</div>
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+            <CardPopularProducts />
+            <div className="bg-gray-500 row-span-3 xl:row-span-6"></div>
+            <div className="bg-gray-500 row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1"></div>
+            <div className="bg-gray-500 row-span-3"></div>
+            <div className="bg-gray-500 md:row-span-1 xl:row-span-2"></div>
+            <div className="bg-gray-500 md:row-span-1 xl:row-span-2"></div>
+            <div className="bg-gray-500 md:row-span-1 xl:row-span-2"></div>
+        </div>
     )
 }
 
