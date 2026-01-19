a4e60161acbb9511226222bb6699d226fe5cb462
Elvin <engrioelvin@gmail.com>
2026-01-19 23:00:40 +0700

    Added Sales, Purchase & Expense Summary to Dashboard

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,14 +1,17 @@
 "use client"
 
+import CardExpenseSummary from "./CardExpenseSummary"
 import CardPopularProducts from "./CardPopularProducts"
+import CardPurchaseSummary from "./CardPurchaseSummary"
+import CardSalesSummary from "./CardSalesSummary"
 
 function Dashboard() {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
             <CardPopularProducts />
-            <div className="bg-gray-500 row-span-3 xl:row-span-6"></div>
-            <div className="bg-gray-500 row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1"></div>
-            <div className="bg-gray-500 row-span-3"></div>
+            <CardSalesSummary />
+            <CardPurchaseSummary />
+            <CardExpenseSummary />
             <div className="bg-gray-500 md:row-span-1 xl:row-span-2"></div>
             <div className="bg-gray-500 md:row-span-1 xl:row-span-2"></div>
             <div className="bg-gray-500 md:row-span-1 xl:row-span-2"></div>
