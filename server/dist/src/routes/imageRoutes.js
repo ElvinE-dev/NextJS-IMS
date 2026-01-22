12bebdfb3994514b5d10bad2e2296876075b0209
Elvin <engrioelvin@gmail.com>
2026-01-22 14:13:06 +0700

    Fixed a little bug

--- server/dist/src/routes/imageRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const imageController_1 = require("../controllers/imageController");
-const router = (0, express_1.Router)();
-router.get('/', imageController_1.getImages);
-exports.default = router;
